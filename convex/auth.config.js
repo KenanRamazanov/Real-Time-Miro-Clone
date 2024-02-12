e23bb4a523a94241fc6293ed3f8b56195c8c7f48
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-12 19:55:44 +0400

    Editing auth config file

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
     providers: [
       {
-        domain: "https://your-issuer-url.clerk.accounts.dev/",
+        domain: "https://loved-anteater-10.clerk.accounts.dev",
         applicationID: "convex",
       },
     ]
