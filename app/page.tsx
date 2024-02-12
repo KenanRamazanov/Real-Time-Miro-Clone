9eb80d244439dfb3aa28ba844848fe0c1b4e9eea
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-12 19:34:03 +0400

    Installation of convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-<div>
-  page
-</div>
+<Button size="sm" variant='destructive'>
+  Click me
+</Button>
   );
 }
