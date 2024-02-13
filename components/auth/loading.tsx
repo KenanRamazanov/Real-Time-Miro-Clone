1d917828f3a6cc2d6067ba07a4eec260cb001d86
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-13 12:59:20 +0400

    Final of auth loading

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import Image from "next/image"
 
-const loading = () => {
-  return (
-    <div>loading</div>
-  )
-}
-
-export default loading
+export const Loading = () => {
+    return(
+        <div className="h-full w-full flex flex-col justify-center items-center">
+         <Image
+         src='/logo.svg'
+         alt="Logo"
+         width={120}
+         height={120}
+         className="animate-pulse duration-700"
+         />
+        </div>
+    );
+};
