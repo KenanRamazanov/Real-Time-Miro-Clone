e78819ff1fbf095d90636e164490349ec3b1fad7
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-14 20:52:33 +0400

    Creating Organization Sidebar

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+export const OrgSidebar = () => {
+    return (
+        <div>
+            OrgSidebar
+        </div>
+    )
+}
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { OrgSidebar } from "./_components/org-sidebar";
 import { Sidebar } from "./_components/sidebar";
 
 interface DashboardLayoutProps{
@@ -10,7 +11,14 @@ const DashboardLayout = ({
    <main className="h-full">
     <Sidebar/>
     <div className="pl-[60px] h-full">
-    {children}
+
+      <div className="flex gap-x-3 h-full">
+        <OrgSidebar/>
+        <div className="h-full flex-1">
+         { /* Add Navbar*/}
+        {children}
+        </div>
+      </div>
     </div>
    </main>
   )
