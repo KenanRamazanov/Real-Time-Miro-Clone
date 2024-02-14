e888b39f5365ad0d2ecc45d374493e81de6a9b5b
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-14 21:26:37 +0400

    Creating NewButton

---
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -0,0 +1,21 @@
+"use client"
+import { Plus } from "lucide-react";
+import { CreateOrganization } from "@clerk/nextjs";
+
+
+import {
+Dialog,
+DialogContent,
+DialogTrigger
+} from "@/components/ui/dialog";
+
+
+export const NewButton = () => {
+
+return (
+    <div>
+        NewButton
+    </div>
+);
+
+};
