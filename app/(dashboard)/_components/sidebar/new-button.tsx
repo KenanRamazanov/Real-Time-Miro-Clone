2272d13f798cd0bdb1b77c1745e45414155aede7
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-14 21:32:37 +0400

    Adding button

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -13,9 +13,17 @@ DialogTrigger
 export const NewButton = () => {
 
 return (
-    <div>
-        NewButton
-    </div>
+    <Dialog>
+        <DialogTrigger asChild>
+           <div className="aspect-square">
+               <button className="bg-white/25 h-full w-full 
+               rounded-md flex items-center justify-center
+                opacity-60 hover:opacity-100 transition">
+                <Plus className="text-white"/>
+               </button>
+           </div>
+        </DialogTrigger>
+    </Dialog>
 );
 
 };
