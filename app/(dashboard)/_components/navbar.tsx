9ac6bc785a1dfa4a2333db2758924b986438ff11
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-14 20:58:40 +0400

    Creating Navbar

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export const Navbar = () => {
+return (
+    <div>
+        Navbar
+    </div>
+)
+    
+}
--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -2,7 +2,7 @@
 
 export const OrgSidebar = () => {
     return (
-        <div>
+        <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5 bg-red-500">
             OrgSidebar
         </div>
     )
