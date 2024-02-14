ba3cfe231feb35e9ad80cdb1bf4775d8d9e8091f
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-14 21:10:32 +0400

    Adding Logout button

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,16 @@
 "use client"
 
+import { UserButton } from "@clerk/nextjs";
+
 export const Navbar = () => {
 return (
     <div className="flex items-center gap-x-4 p-5 bg-green-500">
-        Navbar
+        <div className="hidden lg:flex lg:flex-1 bg-yellow-500">
+        Search
+        </div>
+
+        <UserButton/>
+
     </div>
 );
 };
