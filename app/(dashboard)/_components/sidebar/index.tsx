4c09642254b83230870b0487c2ff5bce89e44d07
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-14 20:41:05 +0400

    Final of Sidebar

--- app/(dashboard)/_components/sidebar/index.tsx
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -1,8 +1,8 @@
 export const Sidebar = () => {
 
     return (
-        <div>
-            Sidebar
-        </div>
+        <aside className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">
+            Side
+        </aside>
     );
 };
