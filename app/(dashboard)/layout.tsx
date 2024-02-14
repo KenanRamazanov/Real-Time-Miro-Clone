9a49fd94b9d70cf3396a9c566ef84a2f9fe44322
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-14 20:34:41 +0400

    Creating Sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Sidebar } from "./_components/sidebar";
 
-const layout = () => {
+interface DashboardLayoutProps{
+    children: React.ReactNode;
+};
+const DashboardLayout = ({
+    children,
+  }: DashboardLayoutProps) => {
   return (
-    <div>layout</div>
+   <main className="h-full">
+    <Sidebar/>
+    <div className="pl-[60px] h-full">
+    {children}
+    </div>
+   </main>
   )
 }
 
-export default layout
+export default DashboardLayout;
