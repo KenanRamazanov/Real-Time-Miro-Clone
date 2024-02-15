879b6dc81938c164b7d7c6c7b85cce143014ff4a
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-15 19:20:57 +0400

    Adding Hint to new button

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -8,6 +8,7 @@ Dialog,
 DialogContent,
 DialogTrigger
 } from "@/components/ui/dialog";
+import { Hint } from "@/components/hint";
 
 
 export const NewButton = () => {
@@ -16,11 +17,13 @@ return (
     <Dialog>
         <DialogTrigger asChild>
            <div className="aspect-square">
+            <Hint label="Create organization">
                <button className="bg-white/25 h-full w-full 
                rounded-md flex items-center justify-center
                 opacity-60 hover:opacity-100 transition">
                 <Plus className="text-white"/>
                </button>
+               </Hint>
            </div>
         </DialogTrigger>
         <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
