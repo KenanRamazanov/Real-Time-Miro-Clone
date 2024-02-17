74a5cb2225c81d7173d952d220de4f28de28ab4c
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-17 20:37:28 +0400

    Creating Search Input

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -4,8 +4,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export const Navbar = () => {
 return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
-        <div className="hidden lg:flex lg:flex-1 bg-yellow-500">
+    <div className="flex items-center gap-x-4 p-5 ">
+        <div className="hidden lg:flex lg:flex-1 ">
         Search
         </div>
         <UserButton/>
