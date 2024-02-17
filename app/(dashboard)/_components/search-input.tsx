e431223064df80a87bfe06be06c4587bd120bba9
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-17 21:11:29 +0400

    Adding functionality to search-input

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -1,4 +1,9 @@
 "use client"
+
+import qs from "query-string";
+import { Search } from "lucide-react";
+import { useDebounce } from "usehooks-ts";
+import { useRouter } from "next/navigation";
 export const SearchInput = () => {
   return (
     <div>SearchInput</div>
