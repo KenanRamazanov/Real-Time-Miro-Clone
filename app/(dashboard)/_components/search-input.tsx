094ca131b2ecc1485b9a689cbaa5b323efdc357b
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-18 21:14:16 +0400

    Adding shadcn input

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -2,8 +2,13 @@
 
 import qs from "query-string";
 import { Search } from "lucide-react";
-import { useDebounce } from "usehooks-ts";
+import { useDebounce } from 'usehooks-ts'
 import { useRouter } from "next/navigation";
+import {
+  ChangeEvent,
+  useEffect,
+  useState,
+} from "react";
 export const SearchInput = () => {
   return (
     <div>SearchInput</div>
