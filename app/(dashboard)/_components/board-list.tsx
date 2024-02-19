977e7612fc6d094ebf43aa7ed58204f29d1751f9
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-19 19:21:11 +0400

    Adding functionality to BoardList

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -16,6 +16,29 @@ export const BoardList = ({
   }: BoardListProps) => {
     const data = []
 
+    if (!data?.length && query.search) {
+        return (
+            <div>
+                Try searching for something else
+            </div>
+        )
+      }
+    
+      if (!data?.length && query.favorites) {
+        return (
+            <div>
+                No favorites
+            </div>
+        )
+      }
+    
+      if (!data?.length) {
+        return (
+            <div>
+                No boards at all
+            </div>
+        )
+      }
     return (
         <div>
           {JSON.stringify(query)}
