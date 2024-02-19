7e8d33158c19e8ff8ea053e278f4d3ebd1f4eabc
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-19 18:57:48 +0400

    Editing page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,26 @@
+"use client"
 import React from 'react'
 import { EmptyOrg } from './_components/empty-org';
+import { useOrganization } from '@clerk/nextjs';
 
-const DashboardPage = () => {
+interface DashboardPageProps{
+  searchParams: {
+    search?: string;
+    favorites?: string
+  };
+};
+
+const DashboardPage = ({
+  searchParams,
+}: DashboardPageProps) => {
+  const { organization } = useOrganization();
   return (
     <div className='flex-1 h-[calc(100%-80px)] p-6'>
-        <EmptyOrg/>
+       {!organization ? (
+        <EmptyOrg />
+      ) : (
+    <p>Board List</p>
+      )}
     </div>
   );
 };
