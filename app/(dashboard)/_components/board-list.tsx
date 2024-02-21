d8fe3979f7fff97bc553640ae743b1cab2c80716
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-21 21:24:31 +0400

    Adding BoardCard to board-list

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -48,6 +48,21 @@ if (data === undefined) {
       <h2 className="text-3xl">
       {query.favorites ? "Favorite boards" : "Team boards"}
       </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+      {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavorite={board.isFavorite}
+          />
+        ))}
+      </div>
         </div>
     )
   }  
