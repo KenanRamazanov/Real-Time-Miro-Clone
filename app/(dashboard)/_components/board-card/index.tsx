f8381a2acc5a6c3d7c2d1d843c1e27eae6912f10
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-22 20:18:28 +0400

    Editing index

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,6 +1,15 @@
 "use client";
 
-export const BoardCard = () => {
+
+interface BoardCardProps {
+    id: string;
+    title: string;
+}
+
+export const BoardCard: React.FC<BoardCardProps> = ({
+    id,
+    title
+}) => {
     return (
         <div>
             Board Card
