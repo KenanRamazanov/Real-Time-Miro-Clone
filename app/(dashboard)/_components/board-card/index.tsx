230ae1e78531ab50a09cf15b79329aa3f197e3d1
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-22 20:24:30 +0400

    Adding image

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 
 
@@ -28,7 +29,12 @@ export const BoardCard: React.FC<BoardCardProps> = ({
       <Link href={ `/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
          <div className="relative flex-1 bg-amber-50">
-
+             <Image
+              src={imageUrl}
+              alt={title}
+              fill
+              className="object-fit"
+             />
          </div>
        </div>
       </Link>
