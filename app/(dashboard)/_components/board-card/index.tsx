5bf3ac2e9d1a657e40f8bd3d9f69c8e61d0a3828
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-22 20:23:27 +0400

    Creating Board design

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,18 +1,36 @@
 "use client";
 
+import Link from "next/link";
+
 
 interface BoardCardProps {
     id: string;
     title: string;
+    authorName: string;
+    authorId: string;
+    createdAt: string;
+    imageUrl: string;
+    orgId: string;
+    isFavorite: boolean;
 }
 
 export const BoardCard: React.FC<BoardCardProps> = ({
     id,
-    title
+    title,
+    authorName,
+    authorId,
+    createdAt,
+    imageUrl,
+    orgId,
+    isFavorite
 }) => {
     return (
-        <div>
-            Board Card
-        </div>
+      <Link href={ `/board/${id}`}>
+      <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+         <div className="relative flex-1 bg-amber-50">
+
+         </div>
+       </div>
+      </Link>
     );
 };
