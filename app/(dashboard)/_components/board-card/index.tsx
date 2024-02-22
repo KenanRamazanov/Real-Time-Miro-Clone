f1d3897982ef7a8b3e0c69641168e8e1ae35e69f
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-22 20:31:52 +0400

    Adding functionality to index

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Overlay } from "./overlay";
 import { formatDistanceToNow } from "date-fns";
+import { useAuth } from "@clerk/nextjs";
 
 interface BoardCardProps {
     id: string;
@@ -26,6 +27,11 @@ export const BoardCard: React.FC<BoardCardProps> = ({
     orgId,
     isFavorite
 }) => {
+    const { userId } = useAuth();
+    const authorLabel = userId === authorId ? "You" : authorName;
+    const createdAtLabel = formatDistanceToNow(createdAt, {
+      addSuffix: true,
+    });
     return (
       <Link href={ `/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
