51453da0987fc6988f2dc2e6698aac7fb9e29aba
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-22 20:30:14 +0400

    Fixing data-fns error

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Overlay } from "./overlay";
-
+import { formatDistanceToNow } from "date-fns";
 
 interface BoardCardProps {
     id: string;
