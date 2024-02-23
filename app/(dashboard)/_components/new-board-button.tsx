31e1ad0323ad997a2efd8c5d192660d81d45d949
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-23 21:21:10 +0400

    Final of New board button

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -29,8 +29,19 @@ export const NewBoardButton:React.FC<NewBoardButtonProps> = ({
         .catch(() => toast.error("Failed to create board"));
      }
     return (
-        <div>
-            NewBoardButton
-        </div>
-    )
-}
+       <button
+       disabled={pending || disabled}
+       onClick={onClick}
+       className={cn(
+         "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
+         (pending || disabled) && "opacity-75 hover:bg-blue-600 cursor-not-allowed"
+       )}
+       >
+        <div/>
+        <Plus className="h-12 w-12 text-white stroke-1"/>
+        <p className="text-sm text-white font-light">
+             New board
+        </p>
+       </button>
+    );
+};
