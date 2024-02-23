2cdbfbd2e57503537c3d5d79a959a926e28b30eb
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-23 21:35:49 +0400

    Editing board-list

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -24,11 +24,20 @@ export const BoardList = ({
       ...query,
     });
 
-if (data === undefined) {
+if (true) {
   return (
     <div>
-      Loading...x
+    <h2 className="text-3xl">
+      {query.favorites ? "Favorite boards" : "Team boards"}
+    </h2>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+      <NewBoardButton orgId={orgId} disabled />
+      <BoardCard.Skeleton />
+      <BoardCard.Skeleton />
+      <BoardCard.Skeleton />
+      <BoardCard.Skeleton />
     </div>
+  </div>
   )
 }
 
