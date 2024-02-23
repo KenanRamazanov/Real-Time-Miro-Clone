52212c30a4585c1eab9c3a6faa4c7a7bad81b4a2
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-23 21:05:57 +0400

    Final of Footer

--- app/(dashboard)/_components/board-card/footer.tsx
+++ app/(dashboard)/_components/board-card/footer.tsx
@@ -1,10 +1,52 @@
 import { Star } from "lucide-react"
+import { cn } from "@/lib/utils"
+import React from "react";
 
-export const Footer = () => {
-
+interface FooterProps {
+    title: string;
+    authorLabel: string;
+    createdAtLabel: string;
+    isFavorite: boolean;
+    onClick: () => void;
+    disabled: boolean;
+  };
+export const Footer: React.FC<FooterProps> = ({
+    title,
+    authorLabel,
+    createdAtLabel,
+    isFavorite,
+    onClick,
+    disabled
+}) => {
+const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement,MouseEvent>
+) => {
+event.preventDefault();
+event.stopPropagation();
+}
     return (
-        <div>
-            Footer
+        <div className="relative bg-white p-3 ">
+           <p className="text-[13px] truncate max-w-[calc(100%-20px)]">
+            {title}
+           </p>
+           <p className="opacity-0 group-hover:opacity-100 transition-opacity text-[11px] text-muted-foreground truncate">
+            {authorLabel},{createdAtLabel}
+           </p>
+           <button
+           disabled={disabled}
+           onClick={handleClick}
+           className={cn(
+             "opacity-0 group-hover:opacity-100 transition absolute top-3 right-3 text-muted-foreground hover:text-blue-600",
+             disabled && "cursor-not-allowed opacity-75"
+           )}
+           >
+             <Star
+             className={cn(
+                "h-4 w-4",
+                isFavorite && "fill-blue-600 text-blue-600"
+              )} 
+             />
+           </button>
         </div>
     )
 }
