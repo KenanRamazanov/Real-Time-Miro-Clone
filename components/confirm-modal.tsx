337a60d0fec6b6633c32df052b92e62c40163db5
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-26 20:38:08 +0400

    Fixing error

--- components/confirm-modal.tsx
+++ components/confirm-modal.tsx
@@ -1,60 +1,60 @@
-"use client"
+"use client";
 
 import {
-AlertDialog,
-AlertDialogAction,
-AlertDialogCancel,
-AlertDialogContent,
-AlertDialogDescription,
-AlertDialogFooter,
-AlertDialogHeader,
-AlertDialogTitle,
-AlertDialogTrigger
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 
 interface ConfirmModalProps {
-    children:React.ReactNode;
-    onConfirm: () => void;
-    disabled?:boolean;
-    header: string;
-    description?:string;
+  children: React.ReactNode;
+  onConfirm: () => void;
+  disabled?: boolean;
+  header: string;
+  description?: string;
 };
 
 export const ConfirmModal = ({
-    children,
-    onConfirm,
-    disabled,
-    header,
-    description,
+  children,
+  onConfirm,
+  disabled,
+  header,
+  description,
 }: ConfirmModalProps) => {
-const handleConfirm = () => {
+  const handleConfirm = () => {
     onConfirm();
-};
-return (
+  };
+
+  return (
     <AlertDialog>
-        <AlertDialogTrigger asChild>
-            {children}
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-            <AlertDialogHeader>
-                <AlertDialogTitle>
-                    {header}
-                </AlertDialogTitle>
-                <AlertDialogDescription>
-                    {description}
-                </AlertDialogDescription>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-            <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogTrigger asChild>
+        {children}
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            {header}
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            {description}
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
           <AlertDialogAction
             disabled={disabled}
             onClick={handleConfirm}
           >
             Confirm
           </AlertDialogAction>
-            </AlertDialogFooter>
-        </AlertDialogContent>
+        </AlertDialogFooter>
+      </AlertDialogContent>
     </AlertDialog>
-);
-
+  );
 };
