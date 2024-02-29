09b4923fdc952c9148d6f2e1eaf9a6c07b73159c
kenan551 <kenan.ramazanov1104@gmail.com>
2024-02-29 21:21:38 +0400

    Adding unfavorite api to board

--- convex/board.ts
+++ convex/board.ts
@@ -120,3 +120,39 @@ export const remove = mutation({
     return board;
   },
 });
+
+export const unfavorite = mutation({
+  args: { id: v.id("boards") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const board = await ctx.db.get(args.id);
+
+    if (!board) {
+      throw new Error("Board not found");
+    }
+
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) => 
+        q
+          .eq("userId", userId)
+          .eq("boardId", board._id)
+      )
+      .unique();
+
+    if (!existingFavorite) {
+      throw new Error("Favorited board not found");
+    }
+
+    await ctx.db.delete(existingFavorite._id);
+
+    return board;
+  },
+});
